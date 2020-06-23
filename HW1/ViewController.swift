76e2c5ce10446ea6feb3c758d485bed530889683
andy <andy870622@gmail.com>
2020-06-23 17:40:26 +0800

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by User15 on 2020/6/23.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
